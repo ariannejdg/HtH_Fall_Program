e4eb5bd48ca6880e0934e4686fdae090aff7bea9
ariannejdg <108510072+ariannejdg@users.noreply.github.com>
2022-10-06 18:38:52 -0700

    completing first python activity files

---
+++ Getting-Started-with-Python-ariannejdg/about_me.py
@@ -0,0 +1,12 @@
+name = "Arianne de Guzman"
+preferred_name = "Arianne"
+age = 16
+have_python_experience = True
+favorite_hobbies = ["Reading", "Listening to music", "Baking"]
+favorites = {
+    'movie': '10 Things I Hate About You',
+    'food': 'Kare Kare',
+    'song': 'Self Control by Frank Ocean'
+}
+
+all_vars = dict(vars())
