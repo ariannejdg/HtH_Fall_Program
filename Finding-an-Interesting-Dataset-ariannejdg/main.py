235c1d6f1e0c0fdfbbc07eeb6837ec41cdcc793b
ariannejdg <108510072+ariannejdg@users.noreply.github.com>
2022-12-14 07:02:40 -0800

    New interesting dataset

--- Finding-an-Interesting-Dataset-ariannejdg/main.py
+++ Finding-an-Interesting-Dataset-ariannejdg/main.py
@@ -3,4 +3,4 @@ import pandas
 pandas.options.display.max_columns = None
 pandas.options.display.max_rows = None
 
-netflix_titles_data = pandas.read_csv("netflix_titles.csv")
+ph_covid_expenses_data = pandas.read_csv("covid_expenses1.csv")
